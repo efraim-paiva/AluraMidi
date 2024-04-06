ce4e555ed49d864cd425b8d44a223e5b2942608a
efraim-paiva <efgaroto@gmail.com>
2024-04-06 19:54:50 -0300

    Add each sound function

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
-function tocarSom(id){
-    
-if (id == 1){
-    document.querySelector('#som_tecla_pom').play()
-} else if (id == 2){
-    document.querySelector('#som_tecla_clap').play()
-} else if (id == 3){
-    document.querySelector('#som_tecla_tim').play()
-} else if (id == 4){
-    document.querySelector('#som_tecla_puff').play()
-} else if (id == 5){
+function teclaPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function teclaClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function teclaTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function teclaPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function teclaSplash() {
     document.querySelector('#som_tecla_splash').play()
-} else if (id == 6){
-    document.querySelector('#som_tecla_toim').play()
-} else if (id == 7){
-    document.querySelector('#som_tecla_psh').play()
-} else if (id == 8){
-    document.querySelector('#som_tecla_tic').play()
-} else if (id == 9){
-    document.querySelector('#som_tecla_tom').play()
 }
+
+function teclaToim() {
+    document.querySelector('#som_tecla_toim').play();
 }
+
+function teclaPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function teclaTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function teclaTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+
+
+
+//alternative way of doing it:
+
+// function tocarSom(id) {
+
+//     if (id == 1) {
+//         document.querySelector('#som_tecla_pom').play()
+//     } else if (id == 2) {
+//         document.querySelector('#som_tecla_clap').play()
+//     } else if (id == 3) {
+//         document.querySelector('#som_tecla_tim').play()
+//     } else if (id == 4) {
+//         document.querySelector('#som_tecla_puff').play()
+//     } else if (id == 5) {
+//         document.querySelector('#som_tecla_splash').play()
+//     } else if (id == 6) {
+//         document.querySelector('#som_tecla_toim').play()
+//     } else if (id == 7) {
+//         document.querySelector('#som_tecla_psh').play()
+//     } else if (id == 8) {
+//         document.querySelector('#som_tecla_tic').play()
+//     } else if (id == 9) {
+//         document.querySelector('#som_tecla_tom').play()
+//     }
+// }
